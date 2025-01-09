4c58931d6193b041ca633fb9bbfe41865cd84eed
fiyin <lolafiyin@gmail.com>
2025-01-09 17:49:22 +0100

    debugging internal server error

--- inventory_management_API/settings.py
+++ inventory_management_API/settings.py
@@ -32,7 +32,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(' ') if not DEBUG else []
+ALLOWED_HOSTS = [
+    'inventory-management-api-lel0.onrender.com',
+    'localhost',  # If running locally
+    '127.0.0.1',  # If running locally
+]
 
 # Cookie Security Settings
 CSRF_COOKIE_SECURE = True
